15269255a4571ce6d99dffb31e876a8f3bfd7e24
Yves Siegrist <Elektron1c97@gmail.com>
2017-12-05 19:22:05 +0100

    manage embed color with ENV variable

--- app/models/discord.rb
+++ app/models/discord.rb
@@ -2,5 +2,5 @@ module Discord
   ADMIN_ROLES = ENV.fetch('DISCORD_ADMIN_ROLES', '')
                    .split(',')
                    .map(&:to_i)
-  EMBED_COLOR = '00EE00'.freeze
+  EMBED_COLOR = ENV.fetch('DISCORD_EMBED_COLOR', '00EE00').freeze
 end
